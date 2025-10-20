54289ac236c88ac9ee6935efbe484712792bbf52
isabellwang <isabell.wang@mail.utoronto.ca>
2025-10-20 18:17:41 -0400

    implemented prepareSuccessView for #2

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -13,7 +13,7 @@ public class Main {
                 .addLoginUseCase()
                 .addChangePasswordUseCase()
                 .addLogoutUseCase()
-                .b uild();
+                .build();
 
         application.pack();
         application.setLocationRelativeTo(null);
